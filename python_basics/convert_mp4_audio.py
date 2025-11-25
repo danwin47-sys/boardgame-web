55b6f63640461cb3ef23bd79104fcc8e29382446
danwin47-sys <danwin47@gmail.com>
2025-11-25 12:43:30 +0800

    Refactor: Move python_basics to separate repository

--- python_basics/convert_mp4_audio.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-input_path = r"C:\Users\judy\Desktop\test\video1.mp4"
-output_path = r"C:\Users\judy\Desktop\test\video1.mp3"
-
-video = VideoFileClip(input_path)
-video.audio.write_audiofile(output_path)
